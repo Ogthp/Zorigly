4e8fb81b929c4ff8b24db7aa0d8d1c13f6999fe5
Ogthp <orgil.giant@gmail.com>
2024-12-22 15:49:50 +0800

    first commit

---
+++ pagenav.js
@@ -0,0 +1,38 @@
+function navigateToHome(){
+        window.location.href = 'home.html'
+}
+
+function navigateToLesson(){
+        window.location.href = 'lessons.html'
+}
+
+function navigateToSource(){
+        window.location.href = 'sources.html'
+}
+
+function navigateToEvent(){
+        window.location.href = 'event.html'
+}
+
+function navigateToAbout(){
+        window.location.href = 'about.html'
+}
+
+function navigateToContact(){
+        window.location.href = 'contact.html'
+}
+
+
+const scrollToTopBtn = document.getElementsByClassName('');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
